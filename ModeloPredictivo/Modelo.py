4fb098b93cd1a684783e5f068635a94ae3d9bcb4
diegolamus <dalumes95@gmail.com>
2018-06-12 15:28:09 -0500

    Se agregaron los modelos pre entrenados que van a ser usados para transfer learning y se contruyeron las capas densas que le siguien.

--- ModeloPredictivo/Modelo.py
+++ ModeloPredictivo/Modelo.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from keras.applications import VGG16, VGG19, ResNet50, InceptionV3, InceptionResNetV2
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
@@ -13,8 +14,9 @@ train_images = 1200
 test_images = 200
 class_mode = 'binary'
 
-# Hiper Parametros de la CNN
+# Hiper Parametros entrenamiento
 epochs = 50
+conv_trainable_layers =0
 
 # Generador de datos para transformar y redimensionar las imagenes de entrenamiento existentes
 # Salida: 4 posibles transformaciones de la imagen original
@@ -47,16 +49,28 @@ test_flow = test_datagen.flow_from_directory(
         batch_size=batch_size,
         class_mode=class_mode)
 
+# Cargar modelo pre-entrenado (Transfer Learning). Solo se carga uno.
+#conv_layers = VGG16(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
+#conv_layers = VGG19(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
+conv_layers = ResNet50(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
+#conv_layers = InceptionV3(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
+#conv_layers = InceptionResNetV2(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
+
+# Congelar las capas que no se quieren entrenar
+for layer in conv_layers.layers[:-conv_trainable_layers]:
+    layer.trainable = False
+
+#Imprimir las capas que se van a entrenar
+for layer in conv_layers.layers:
+    print(layer, layer.trainable)
+    
 # Crear modelo usando keras
 model = Sequential()
-
-# Construir el modelo utilizando keras
-
-
-
-
-
-
+model.add(conv_layers) # Se agrega el modelo pre cargado al actual
+model.add(layers.Flatten())
+model.add(layers.Dense(1024, activation='relu'))
+model.add(layers.Dropout(0.5))
+model.add(Dense(1, activation='sigmoid'))
 
 # Compilar modelo
 # Parametros
