0f8431b83b12e5889439507bc55c6eda0904982b
diegolamus <dalumes95@gmail.com>
2018-06-22 18:21:14 -0500

    Pantalla de inicio creada

--- AplicacionMovil/app/app/src/main/java/co/edu/icesi/leishmaniasisprediagnstico/VistaInicial.java
+++ AplicacionMovil/app/app/src/main/java/co/edu/icesi/leishmaniasisprediagnstico/VistaInicial.java
@@ -2,12 +2,33 @@ package co.edu.icesi.leishmaniasisprediagnstico;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class VistaInicial extends AppCompatActivity {
+
+    Button boton_instrucciones;
+    Button boton_seleccionar_foto;
+    Button boton_tomar_foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_inicial);
+        boton_instrucciones = (Button) findViewById(R.id.instrucciones);
+        boton_seleccionar_foto = (Button) findViewById(R.id.seleccionar_foto);
+        boton_tomar_foto = (Button) findViewById(R.id.tomar_foto);
+    }
+
+    public void desplegarInstrucciones(View v){
+        //TODO
+    }
+
+    public void tomarFoto(View v){
+        //TODO
+    }
+
+    public void seleccionarFoto(View v){
+        //TODO
     }
 }
