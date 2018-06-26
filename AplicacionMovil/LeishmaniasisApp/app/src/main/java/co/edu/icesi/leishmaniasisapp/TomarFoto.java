99011e75bff1dee1ea61a1931495161ba48197fe
diegolamus <dalumes95@gmail.com>
2018-06-26 16:27:14 -0500

    Enfocar acomodado

--- AplicacionMovil/LeishmaniasisApp/app/src/main/java/co/edu/icesi/leishmaniasisapp/TomarFoto.java
+++ AplicacionMovil/LeishmaniasisApp/app/src/main/java/co/edu/icesi/leishmaniasisapp/TomarFoto.java
@@ -1,18 +1,13 @@
 package co.edu.icesi.leishmaniasisapp;
 
-import android.content.Context;
-import android.content.res.Configuration;
+import android.annotation.SuppressLint;
 import android.graphics.Rect;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
 import android.view.View;
 import android.widget.FrameLayout;
-
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,6 +20,7 @@ public class TomarFoto extends AppCompatActivity {
     FrameLayout layoutCamera;
     MostrarCamara mostrarCamara;
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,46 +28,11 @@ public class TomarFoto extends AppCompatActivity {
         layoutCamera = findViewById(R.id.layoutCamera);
         camera = Camera.open();
         mostrarCamara = new MostrarCamara(this, camera);
-
         mostrarCamara.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (camera != null) {
-                    camera.cancelAutoFocus();
-                    Rect focusRect = calculateFocusArea(motionEvent.getX(), motionEvent.getY());
-                    Camera.Parameters parameters = camera.getParameters();
-                    if (parameters.getFocusMode().equals(Camera.Parameters.FOCUS_MODE_AUTO)) {
-                        parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
-                    }
-                    if (parameters.getMaxNumFocusAreas() > 0) {
-                        List<Camera.Area> mylist = new ArrayList<Camera.Area>();
-                        mylist.add(new Camera.Area(focusRect, 1000));
-                        parameters.setFocusAreas(mylist);
-                    }
-
-                    try {
-                        camera.cancelAutoFocus();
-                        camera.setParameters(parameters);
-                        camera.startPreview();
-                        camera.autoFocus(new Camera.AutoFocusCallback() {
-                            @Override
-                            public void onAutoFocus(boolean success, Camera camera) {
-                                if (camera.getParameters().getFocusMode().equals(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
-                                    Camera.Parameters parameters = camera.getParameters();
-                                    parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
-                                    if (parameters.getMaxNumFocusAreas() > 0) {
-                                        parameters.setFocusAreas(null);
-                                    }
-                                    camera.setParameters(parameters);
-                                    camera.startPreview();
-                                }
-                            }
-                        });
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-
-
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    focusOnTouch(motionEvent);
                 }
                 return true;
             }
@@ -79,14 +40,13 @@ public class TomarFoto extends AppCompatActivity {
         layoutCamera.addView(mostrarCamara);
     }
 
-
     public void onClick_tomarFoto(View v) {
         //Tomar la foto
         if (camera != null) {
             camera.takePicture(null, null, pictureCallBack);
         }
-
     }
+
     //Recuperar y guardar la foto
     Camera.PictureCallback pictureCallBack = new Camera.PictureCallback() {
         @Override
@@ -96,8 +56,41 @@ public class TomarFoto extends AppCompatActivity {
     };
 
     //Enfocar
-    private void focusOnTouch( MotionEvent motionEvent){
-
+    private void focusOnTouch(MotionEvent motionEvent) {
+        if (camera != null) {
+            camera.cancelAutoFocus();
+            Rect focusRect = calculateFocusArea(motionEvent.getX(), motionEvent.getY());
+            Camera.Parameters parameters = camera.getParameters();
+            if (parameters.getFocusMode().equals(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            }
+            if (parameters.getMaxNumFocusAreas() > 0) {
+                List<Camera.Area> mylist = new ArrayList<>();
+                mylist.add(new Camera.Area(focusRect, 1000));
+                parameters.setFocusAreas(mylist);
+            }
+            try {
+                camera.cancelAutoFocus();
+                camera.setParameters(parameters);
+                camera.startPreview();
+                camera.autoFocus(new Camera.AutoFocusCallback() {
+                    @Override
+                    public void onAutoFocus(boolean success, Camera camera) {
+                        if (camera.getParameters().getFocusMode().equals(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                            Camera.Parameters parameters = camera.getParameters();
+                            parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+                            if (parameters.getMaxNumFocusAreas() > 0) {
+                                parameters.setFocusAreas(null);
+                            }
+                            camera.setParameters(parameters);
+                            camera.startPreview();
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
     private Rect calculateFocusArea(float x, float y) {
         int left = clamp(Float.valueOf((x / mostrarCamara.getWidth()) * 2000 - 1000).intValue(), FOCUS_AREA_SIZE);
@@ -106,73 +99,20 @@ public class TomarFoto extends AppCompatActivity {
     }
     private int clamp(int touchCoordinateInCameraReper, int focusAreaSize) {
         int result;
-        if (Math.abs(touchCoordinateInCameraReper)+focusAreaSize/2>1000){
-            if (touchCoordinateInCameraReper>0){
-                result = 1000 - focusAreaSize/2;
+        if (Math.abs(touchCoordinateInCameraReper) + focusAreaSize / 2 > 1000) {
+            if (touchCoordinateInCameraReper > 0) {
+                result = 1000 - focusAreaSize / 2;
             } else {
-                result = -1000 + focusAreaSize/2;
+                result = -1000 + focusAreaSize / 2;
             }
-        } else{
-            result = touchCoordinateInCameraReper - focusAreaSize/2;
+        } else {
+            result = touchCoordinateInCameraReper - focusAreaSize / 2;
         }
         return result;
     }
 }
 
-class MostrarCamara extends SurfaceView implements SurfaceHolder.Callback {
-    Camera camera;
-    SurfaceHolder holder;
 
-    public MostrarCamara(Context context, Camera camera) {
-        super(context);
-        this.camera = camera;
-        holder = getHolder();
-        holder.addCallback(this);
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        camera.stopPreview();
-        camera.release();
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        Camera.Parameters params = camera.getParameters();
-        //Establecer la maxima resolucion
-        List<Camera.Size> sizes = params.getSupportedPictureSizes();
-        Camera.Size max = null;
-        for (Camera.Size size : sizes)
-            max = size;
-        if(max!=null)
-            params.setPictureSize(max.width, max.height);
-        //Cambiar la orientacion de la camara
-        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
-            params.set("orientation", "portrait");
-            camera.setDisplayOrientation(90);
-            params.setRotation(90);
-        } else {
-            params.set("orientation", "landscape");
-            camera.setDisplayOrientation(0);
-            params.setRotation(0);
-        }
-        //Establecer parametros
-        camera.setParameters(params);
-        try {
-            camera.setPreviewDisplay(holder);
-            camera.startPreview();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-    }
-
-}
 
 
 
