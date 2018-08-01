c8bcb08278fcc15ecbb4d2d2bd567c5fc413617e
diegolamus <dalumes95@gmail.com>
2018-08-01 08:29:56 -0500

    Exportar modelo como Json agregado

--- ModeloPredictivo/Modelo.py
+++ ModeloPredictivo/Modelo.py
@@ -4,19 +4,21 @@ from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from keras.callbacks import ModelCheckpoint
 
 # Parametros de la carga de datos
 batch_size = 16
 imgage_size = 300
-train_directory = 'RGB/datos/entrenamiento'
-test_directory = 'RGB/datos/prueba'
-train_images = 1581
-test_images = 395
+train_directory = 'src/datos/entrenamiento'
+test_directory = 'src/datos/prueba'
+train_images = 1618
+test_images = 404
 class_mode = 'binary'
 
 # HiperParametros entrenamiento
 epochs = 1
 guardar_como= 'Prueba_Xception' #Con que nombre se van a aguardar los pesos y exportar la historia
+
 
 def construir_modelo():
     # Cargar modelo pre-entrenado (Transfer Learning). Solo se carga uno.
@@ -28,15 +30,12 @@ def construir_modelo():
     #conv_layers = InceptionResNetV2(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
     #conv_layers = MobileNet(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
     #conv_layers = DenseNet121(weights='imagenet',include_top=False,input_shape=(imgage_size, imgage_size, 3))
-
     # Congelar las capas que no se quieren entrenar
     for layer in conv_layers.layers[:]:
         layer.trainable = False
-
     # Imprimir las capas que se van a entrenar
     for layer in conv_layers.layers:
         print(layer, layer.trainable)
-    
     # Crear modelo usando keras
     modelo = Sequential()
     modelo.add(conv_layers) # Se agrega el modelo pre-cargado al actual
@@ -44,7 +43,9 @@ def construir_modelo():
     modelo.add(Dense(1024, activation='relu'))
     modelo.add(Dropout(0.5))
     modelo.add(Dense(1, activation='sigmoid'))
+    modelo.summary()
     return modelo
+
 
 def entrenar_modelo(modelo):
     # Generador de datos para transformar y redimensionar las imagenes de entrenamiento existentes
@@ -55,11 +56,9 @@ def entrenar_modelo(modelo):
         rescale=1./255,
         horizontal_flip=True,
         vertical_flip=True)
-
     # Generador de datos para redimensionar las imagenes de prueba
     # Salida: imagenes de prueba redimensionadas a 1./255
     test_datagen = ImageDataGenerator(rescale=1./255)
-
     # flujo de datos de entrenamiento
     # Salida: lotes de imagenes transformadas mediante train_datagen con etiquetas
     #         correspondientes al nombre de su subfolder ubicado en train_directory (positivo, negativo).
@@ -68,7 +67,6 @@ def entrenar_modelo(modelo):
         target_size=(imgage_size, imgage_size),  
         batch_size=batch_size,
         class_mode=class_mode)
-
     # flujo de datos de prueba
     # Salida: lotes de imagenes redimensionadas mediante test_datagen con etiquetas
     #         correspondientes al nombre de su subfolder ubicado en train_directory (positivo, negativo).
@@ -77,7 +75,6 @@ def entrenar_modelo(modelo):
         target_size=(imgage_size, imgage_size),
         batch_size=batch_size,
         class_mode=class_mode)
-    
     # Compilar modelo
     # Parametros
     #   funcion de perdida = binary_crossentropy [ y*log(y') + (1-y)*log(1-y') ] y=etuiqueta, y'=predición
@@ -86,40 +83,42 @@ def entrenar_modelo(modelo):
         loss='binary_crossentropy',
         optimizer='rmsprop',
         metrics=['accuracy'])
-
+    #Agregar checkpoint para cargar mejores pesos
+    rutaMejoresPesos='src/pesosEntrenamiento/MejoresPesos_' + guardar_como+ '.h5'
+    checkpoint = ModelCheckpoint(rutaMejoresPesos, monitor='val_acc', verbose=1, save_best_only=True, mode='max')
     # Agregar flujes de entrenamiento y prueba al modelo e inciar el entrenamiento
     historia = modelo.fit_generator(
         generator=train_flow,
         steps_per_epoch=train_images//batch_size,
         epochs=epochs,
         validation_data=test_flow,
-        validation_steps=test_images//batch_size)
+        validation_steps=test_images//batch_size,
+        callbacks=[checkpoint],
+        verbose=1)
     return historia
 
 
-def exportar(modelo,historia):
-    # Guardar los pesos de los parametros resultantes del entrenamiento
-    modelo.save_weights('RGB/pesosEntrenamiento/'+guardar_como+'.h5')
-    print('pesos exportados')
-
+def exportar(historia):
     # Exportar la historia a un archivo de texto
     precision_entrenamiento = historia.history['acc'] # recuperar precision datos de entrenamiento
     precision_validacion = historia.history['val_acc'] # recuperar precision datos de prueba
     archivo = open('RGB/historia/'+guardar_como+'.txt','w')
-    archivo.write('Precisión Entrenamiento,Precisión Prueba')
+    archivo.write('Precisión Entrenamiento,Precisión Prueba' + '\n')
     for indice in range(0, epochs):
-        archivo.write(str(precision_entrenamiento[indice])+','+str(precision_validacion[indice]))
+        archivo.write(str(precision_entrenamiento[indice])+','+str(precision_validacion[indice])+ '\n')
     archivo.close()
     print('historia exportada')
 
-    #Exportar el archivo a .db (Archivo usable desde proyecto java)
-    #TODO
-
+    #Exportar modelo a json
+    json_string = model.to_json()
+    text_file = open('src/pesosEntrenamiento/MejoresPesos_' + guardar_como+ ' _json','w')
+    text_file.write(json_string)
+    text_file.close()
 
 def main():
     modelo = construir_modelo()
     historia = entrenar_modelo(modelo)
-    exportar(modelo,historia)
+    exportar_historia(modelo, historia)
 
 
 if __name__ == '__main__':
